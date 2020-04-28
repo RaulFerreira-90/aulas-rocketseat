7c1101e33370e43a39f6e272d7407f1f3d962706
Raul Oliveira Ferreira <raul.oliveiraferreira@gmail.com>
2020-04-28 19:58:11 -0300

    users routes

--- iniciando-back-end/src/routes/index.ts
+++ iniciando-back-end/src/routes/index.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
+
 import appointmentsRouter from './appointmenst.routes';
+import usersRouter from './users.routes';
 
 const routes = Router();
 
 routes.use('/appointments', appointmentsRouter);
+routes.use('/users', usersRouter);
 
 export default routes;
