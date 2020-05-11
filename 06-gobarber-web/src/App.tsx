9f9b0030bdc089ee2d8c523f54497312e662e17f
Raul Oliveira Ferreira <raul.oliveiraferreira@gmail.com>
2020-05-11 15:14:54 -0300

    Style

--- 06-gobarber-web/src/App.tsx
+++ 06-gobarber-web/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import SingIn from './pages/SignIn';
+// import SingUp from './pages/SignUp';
 import GlobalStyle from './styles/global';
 
 const App: React.FC = () => (
   <>
-    <h1>Hello World</h1>
+    <SingIn />
     <GlobalStyle />
   </>
 );
