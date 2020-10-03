f37f9e62d75b2c71e1f6975ef44b4dcf35ca3bfb
Raul Ferreira <raul.oliveiraferreira@gmail.com>
2020-10-03 11:56:31 -0300

    Principio de inversao de dependencia

--- iniciando-back-end/src/modules/appointments/infra/http/routes/appointmenst.routes.ts
+++ iniciando-back-end/src/modules/appointments/infra/http/routes/appointmenst.routes.ts
@@ -1,29 +1,30 @@
 import { Router } from 'express';
-import { getCustomRepository } from 'typeorm';
 import { parseISO } from 'date-fns';
 
-import AppointmentsRepositroy from '@modules/appointments/repositories/AppointmentsRepository';
+import AppointmentsRepositroy from '@modules/appointments/infra/typeorm/repositories/AppointmentsRepository';
 import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 
 const appointmentsRouter = Router();
+const appointmentsRepository = new AppointmentsRepositroy();
 
 appointmentsRouter.use(ensureAuthenticated);
 
-appointmentsRouter.get('/', async (request, response) => {
-  const appointmentsRepository = getCustomRepository(AppointmentsRepositroy);
-  const appointments = await appointmentsRepository.find();
+// appointmentsRouter.get('/', async (request, response) => {
+//   const appointments = await appointmentsRepository.find();
 
-  return response.json(appointments);
-});
+//   return response.json(appointments);
+// });
 
 appointmentsRouter.post('/', async (request, response) => {
   const { provider_id, date } = request.body;
 
   const parsedDate = parseISO(date);
 
-  const createAppointment = new CreateAppointmentService();
+  const createAppointment = new CreateAppointmentService(
+    appointmentsRepository,
+  );
 
   const appointment = await createAppointment.execute({
     date: parsedDate,
